41665c8a33f37a3931e3ab16a04ef1c67fa893a0
mark-mckinney <95238679+mark-mckinney@users.noreply.github.com>
2021-12-01 12:31:15 -0600

    create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+    type = string
+    default = ""
+}
+
+variable "private_key" {
+    type = string
+    default = ""
+}
+
+variable "ssh_key_name" {
+    type = string
+    default = "aws-main-access-key"
+}
+
+
